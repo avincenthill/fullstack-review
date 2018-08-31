559afaf8460da712f6dfd1accade3a0488a0501c
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-08-31 10:09:54 -0700

    Init repo and start script

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ app.get('/repos', function (req, res) {
   // This route should send back the top 25 repos
 });
 
-let port = 1128;
+let port = process.env.PORT || 1337;
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`listening on port ${port}`);
 });
 
